fd4a18290cdae2ce73b279479971324443ebfd0e
anrzgit <109352823+anrzgit@users.noreply.github.com>
2023-03-04 15:17:13 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Check</h1>
+  </div>,
+  document.getElementById("root")
+);
+
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
